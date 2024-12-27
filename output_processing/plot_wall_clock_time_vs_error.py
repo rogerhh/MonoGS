a8b2c5e42a003b637b419bacdc14be07c63eb7fb
rogerhh <rogerhh@umich.edu>
2024-12-27 12:47:54 -0800

    per iteration profiling

--- output_processing/plot_wall_clock_time_vs_error.py
+++ output_processing/plot_wall_clock_time_vs_error.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 import os
 import re
+from output_processing.utils import load_data
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,24 +20,7 @@ if __name__ == "__main__":
 
     print(f"Found {len(files)} files")
 
-    all_profile_data = {}
-
-    for fname in files:
-        # extract last_frame number
-        last_frame = int(re.search(r"frame(\d+).pt", fname).group(1))
-
-        data = torch.load(os.path.join(logdir, fname))
-
-        num_frames = len(data)
-
-        print(f"Loaded {num_frames} frames from {fname}")
-
-        for i in range(num_frames):
-            frame = last_frame - num_frames + 1 + i
-            assert frame not in all_profile_data
-            all_profile_data[frame] = data[i]
-
-    print(all_profile_data.keys())
+    all_profile_data = load_data(logdir)
 
     # Randomly pick a frame to plot
     frame = np.random.choice(list(all_profile_data.keys()))
